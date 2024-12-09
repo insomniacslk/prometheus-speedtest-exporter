1abf9b2cb2f4cf471943c3e5a95211665ae978ad
Andrea Barberio <insomniac@slackware.it>
2024-12-09 23:08:47 +0100

    Added -R for regexp filtering of speedtest servers
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- main.go
+++ main.go
@@ -26,13 +26,14 @@ var (
 	flagSpeedTestCLI      = flag.String("s", "speedtest-cli", "Path to speedtest-cli")
 	flagSpeedTestServerID = flag.Int("S", 0, "Server ID obtained with `speedtest-cli --list`")
 	flagSleepInterval     = flag.Duration("i", 30*time.Minute, "Interval between speedtest executions, expressed as a Go duration string")
-	flagRetryInterval     = flag.Duration("r", 1*time.Minute, "Interval between retries when `speedtest --list` fails to find a server, expressed as a Go duration string")
+	flagRetryInterval     = flag.Duration("r", 1*time.Minute, "Interval between retries when 'speedtest --list' fails to find a server, expressed as a Go duration string")
 	flagInsecure          = flag.Bool("I", false, "Insecure mode: use HTTP instead of HTTPS")
 	flagDebug             = flag.Bool("d", false, "Enable debugging output")
 	flagMaxDistance       = flag.Int("m", 0, "Max distance in km to the speedtest server")
+	flagServerRegexp      = flag.String("R", "", "Regular expression to match the candidate servers")
 )
 
-var errRetryable = fmt.Errorf("speedtest temporarily failed, try again later")
+var errRetryable403 = fmt.Errorf("speedtest temporarily failed for HTTP 403, try again later")
 
 const defaultRetryInterval = 60 * time.Second
 
@@ -113,7 +114,7 @@ func speedtest(cliPath string, serverIDs []int, insecure bool) (*speedTestResult
 			// Forbidden we know something's being updated on the SpeedTest
 			// side, so we can wait and retry
 			if errCode == 403 {
-				return nil, errRetryable
+				return nil, errRetryable403
 			}
 		}
 		if err := scanner.Err(); err != nil {
@@ -138,7 +139,7 @@ type SpeedtestServer struct {
 
 var serverListRegexp = regexp.MustCompile(`(\d+)\) (.+) [[](\d+\.\d+) km[]]`)
 
-func getClosestServers(cliPath string, insecure bool) ([]SpeedtestServer, error) {
+func getServers(cliPath string, insecure bool) ([]SpeedtestServer, error) {
 	args := []string{"--list"}
 	if !insecure {
 		args = append(args, "--secure")
@@ -166,7 +167,7 @@ func getClosestServers(cliPath string, insecure bool) ([]SpeedtestServer, error)
 			// Forbidden we know something's being updated on the SpeedTest
 			// side, so we can wait and retry
 			if errCode == 403 {
-				return nil, errRetryable
+				return nil, errRetryable403
 			}
 		}
 		if err := scanner.Err(); err != nil {
@@ -249,6 +250,14 @@ func main() {
 	if err := prometheus.Register(speedtestPingGauge); err != nil {
 		logrus.Fatalf("Failed to register speedtest ping gauge: %v", err)
 	}
+	var serverRegexp *regexp.Regexp
+	if *flagServerRegexp != "" {
+		rx, err := regexp.Compile(*flagServerRegexp)
+		if err != nil {
+			logrus.Fatalf("Failed to parse server regexp: %v", err)
+		}
+		serverRegexp = rx
+	}
 
 	go func() {
 		for {
@@ -257,21 +266,50 @@ func main() {
 				res *speedTestResult
 				err error
 			)
-			if *flagMaxDistance > 0 {
-				logrus.Infof("Looking for servers within %d km", *flagMaxDistance)
-				allServers, err := getClosestServers(*flagSpeedTestCLI, *flagInsecure)
+			if *flagServerRegexp == "" && *flagMaxDistance == 0 {
+				// run the speedtest without any server preference
+				if *flagSpeedTestServerID != 0 {
+					logrus.Infof("Using server ID %d", *flagSpeedTestServerID)
+					serverIDs = []int{*flagSpeedTestServerID}
+				} else {
+					logrus.Infof("Using random server")
+				}
+			} else {
+				allServers, err := getServers(*flagSpeedTestCLI, *flagInsecure)
 				if err != nil {
-					logrus.Warningf("Failed to get list of closest servers: %v", err)
+					logrus.Warningf("Failed to get list of speedtest servers: %v", err)
 					setError(*speedtestSpeedGauge, speedtestPingGauge)
 					logrus.Infof("Sleeping %s before retrying to get server list...", *flagRetryInterval)
 					time.Sleep(*flagRetryInterval)
 					continue
 				}
-				logrus.Infof("Found %d servers in total", len(allServers))
-				for _, s := range allServers {
-					if s.DistanceKm <= *flagMaxDistance {
-						serverIDs = append(serverIDs, s.ID)
+				logrus.Infof("Found %d total servers (before filtering)", len(allServers))
+				if serverRegexp != nil {
+					// filter servers by regexp first
+					logrus.Infof("Filtering servers matching regexp %q", *flagServerRegexp)
+					var servers []SpeedtestServer
+					for _, s := range allServers {
+						if serverRegexp.MatchString(s.Name) {
+							servers = append(servers, s)
+						}
 					}
+					logrus.Infof("Remaining servers after regexp filtering: %d", len(servers))
+					allServers = servers
+				}
+				if *flagMaxDistance > 0 {
+					logrus.Infof("Filtering servers within %d km", *flagMaxDistance)
+					var servers []SpeedtestServer
+					for _, s := range allServers {
+						if s.DistanceKm <= *flagMaxDistance {
+							servers = append(servers, s)
+						}
+					}
+					logrus.Infof("Remaining servers after distance filtering: %d", len(servers))
+					allServers = servers
+				}
+				// now get the list of server IDs from the filtered servers
+				for _, s := range allServers {
+					serverIDs = append(serverIDs, s.ID)
 				}
 				if len(serverIDs) == 0 {
 					logrus.Warningf("No server found within %d km", *flagMaxDistance)
@@ -280,19 +318,16 @@ func main() {
 					time.Sleep(*flagRetryInterval)
 					continue
 				}
-			} else {
-				if *flagSpeedTestServerID != 0 {
-					logrus.Infof("Using server ID %d", *flagSpeedTestServerID)
-					serverIDs = []int{*flagSpeedTestServerID}
-				} else {
-					logrus.Infof("Using random server")
+				logrus.Infof("Found %d servers after filtering", len(allServers))
+				for idx, s := range allServers {
+					logrus.Infof("%d) (ID: %d) %s, %d km", idx+1, s.ID, s.Name, s.DistanceKm)
 				}
 			}
-			logrus.Infof("Running speed test...")
+			logrus.Infof("Running speed test with server IDs %v", serverIDs)
 			res, err = speedtest(*flagSpeedTestCLI, serverIDs, *flagInsecure)
 			if err != nil {
-				if err == errRetryable {
-					logrus.Warningf("Retryable error, sleeping for %s", defaultRetryInterval)
+				if err == errRetryable403 {
+					logrus.Warningf("Retryable HTTP 403 error, sleeping for %s: %v", defaultRetryInterval, err)
 					time.Sleep(defaultRetryInterval)
 					continue
 				}
